318c5f80b6d0633ea0f4892d45b2e2ef981e773b
Marcos Ibarra <mni_1996@yahoo.com.ar>
2019-11-27 17:12:58 -0300

    funcionan los test

--- src/test/java/ProviderTest.kt
+++ src/test/java/ProviderTest.kt
@@ -4,7 +4,6 @@ import Modelo.Enums.DiasDeSemana
 import Modelo.Enums.Horarios
 import Modelo.Enums.MedidorDelLapso
 import Modelo.Provider
-import Modelo.Posicion
 import junit.framework.Assert.assertEquals
 import Modelo.Menu
 import Modelo.Promocion
@@ -17,13 +16,13 @@ import kotlin.test.assertFails
 class ProviderTest{
     @Test
     fun registroMenuExitoso(){
-        var prov =Provider("el Buen Comer","url","Quilmes", Posicion(),
+        var prov =Provider("el Buen Comer","url","Quilmes", "asd",
                 "add","servicio de comidas"
                 ,null,"sdhad@gmail.com",45434542, mutableListOf(Horarios.Morning, Horarios.Afternoon),
                 mutableListOf(DiasDeSemana.Lunes, DiasDeSemana.Martes, DiasDeSemana.Miercoles,
                         DiasDeSemana.Jueves, DiasDeSemana.Viernes),20)
         var menu= Menu("arroz","arroz blancooooooooooooooo", Categoria.Vegano
-                ,10,DateTime(), Horarios.Morning, TimeLapsus(10, MedidorDelLapso.Minutes),50.00
+                ,10,DateTime(), Horarios.Morning, DateTime.now().plusDays(10),50.00
                 , Promocion(11,30.00),null
                 ,100,true)
         prov.addMenu(menu)
@@ -32,13 +31,13 @@ class ProviderTest{
 
     @Test
     fun registroMenuFallido(){
-        var prov =Provider("el Buen Comer","url","Quilmes", Posicion(),
+        var prov =Provider("el Buen Comer","url","Quilmes", "asd",
                 "add","servicio de comidas"
                 ,null,"sdhad@gmail.com",45434542, mutableListOf(Horarios.Morning, Horarios.Afternoon),
                 mutableListOf(DiasDeSemana.Lunes, DiasDeSemana.Martes, DiasDeSemana.Miercoles,
                         DiasDeSemana.Jueves, DiasDeSemana.Viernes),20)
         var menu= Menu("arroz","arroz blancooooooooooooooo", Categoria.Vegano
-                ,10,DateTime(), Horarios.Morning, TimeLapsus(10, MedidorDelLapso.Minutes),50.00
+                ,10,DateTime(), Horarios.Morning, DateTime.now().plusDays(10),50.00
                 , Promocion(3,30.00),null
                 ,100,true)
         assertFails(" el menu no cumple con los requerimientos minimos de aceptacion",{prov.addMenu(menu)})
@@ -46,10 +45,10 @@ class ProviderTest{
     @Test
     fun quitoMenuCaducoYNoPuedo(){
         var menu= Menu("arroz","arroz blancooooooooooooooo", Categoria.Vegano
-                ,10, DateTime(), Horarios.Morning, TimeLapsus(10, MedidorDelLapso.Minutes),50.00
+                ,10, DateTime(), Horarios.Morning, DateTime.now().plusDays(10),50.00
                 , Promocion(11,30.00),null
                 ,100,true)
-        var prov =Provider("el Buen Comer","url","Quilmes", Posicion(),
+        var prov =Provider("el Buen Comer","url","Quilmes", "asd",
                 "add","servicio de comidas"
                 ,null,"sdhad@gmail.com",45434542, mutableListOf(Horarios.Morning, Horarios.Afternoon),
                 mutableListOf(DiasDeSemana.Lunes, DiasDeSemana.Martes, DiasDeSemana.Miercoles,
