4a81b0c52d9c218ff7081ad4d68a1fc8309ef44f
Zudotaky <fire123@live.com.ar>
2019-12-15 12:01:38 -0300

    se cambia el nombre del pakage java por kotlin

---
+++ src/main/kotlin/Modelo/App.kt
@@ -0,0 +1,16 @@
+package Modelo
+
+import Api.FireBaseService
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+    FireBaseService().initialize()
+}
+
